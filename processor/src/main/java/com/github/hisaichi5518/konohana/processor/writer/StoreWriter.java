43dc6a1a12cafe76f352444068eca1ecbe5d3314
hisaichi5518 <hisaichi5518@gmail.com>
2017-09-20 16:11:03 +0900

    Rename methods

--- processor/src/main/java/com/github/hisaichi5518/konohana/processor/writer/StoreWriter.java
+++ processor/src/main/java/com/github/hisaichi5518/konohana/processor/writer/StoreWriter.java
@@ -8,9 +8,6 @@ import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.ParameterSpec;
 import com.squareup.javapoet.TypeSpec;
 
-import java.util.Collections;
-import java.util.List;
-
 import javax.lang.model.element.Modifier;
 
 public class StoreWriter {
@@ -19,30 +16,21 @@ public class StoreWriter {
 
         TypeSpec typeSpec = TypeSpec.classBuilder(storeDefinition.getStoreClassName())
                 .addSuperinterface(storeDefinition.getInterfaceName())
-                .addFields(buildFields(storeDefinition))
-                .addMethods(buildConstructors(storeDefinition))
+                .addField(buildField())
+                .addMethod(buildConstructor(storeDefinition))
                 .build();
     }
 
-    private static List<FieldSpec> buildFields(StoreDefinition storeDefinition) {
-        List<FieldSpec> specs = Collections.emptyList();
-
-        specs.add(FieldSpec.builder(AndroidTypes.SharedPreferences, "prefs")
+    private static FieldSpec buildField() {
+        return FieldSpec.builder(AndroidTypes.SharedPreferences, "prefs")
                 .addModifiers(Modifier.PRIVATE, Modifier.FINAL)
-                .build());
-
-        return specs;
-
+                .build();
     }
 
-    private static List<MethodSpec> buildConstructors(StoreDefinition storeDefinition) {
-        List<MethodSpec> specs = Collections.emptyList();
-
-        specs.add(MethodSpec.constructorBuilder()
+    private static MethodSpec buildConstructor(StoreDefinition storeDefinition) {
+        return MethodSpec.constructorBuilder()
                 .addParameter(ParameterSpec.builder(AndroidTypes.Context, "context").addAnnotation(AnnotationTypes.NonNull).build())
                 .addStatement("this.prefs = context.getSharedPreferences($S, $L)", storeDefinition.getPrefsFileName(), storeDefinition.getPrefsMode())
-                .build());
-
-        return specs;
+                .build();
     }
 }

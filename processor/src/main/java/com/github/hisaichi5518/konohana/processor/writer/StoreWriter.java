b1e95838098ec22cadb7e9fce8c0d1770b95ea30
hisaichi5518 <hisaichi5518@gmail.com>
2017-09-20 16:17:38 +0900

    write *_Store class

--- processor/src/main/java/com/github/hisaichi5518/konohana/processor/writer/StoreWriter.java
+++ processor/src/main/java/com/github/hisaichi5518/konohana/processor/writer/StoreWriter.java
@@ -4,9 +4,12 @@ import com.github.hisaichi5518.konohana.processor.definition.StoreDefinition;
 import com.github.hisaichi5518.konohana.processor.types.AndroidTypes;
 import com.github.hisaichi5518.konohana.processor.types.AnnotationTypes;
 import com.squareup.javapoet.FieldSpec;
+import com.squareup.javapoet.JavaFile;
 import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.ParameterSpec;
 import com.squareup.javapoet.TypeSpec;
+
+import java.io.IOException;
 
 import javax.lang.model.element.Modifier;
 
@@ -19,6 +22,14 @@ public class StoreWriter {
                 .addField(buildField())
                 .addMethod(buildConstructor(storeDefinition))
                 .build();
+
+        try {
+            JavaFile.builder(storeDefinition.getInterfaceName().packageName(), typeSpec)
+                    .build()
+                    .writeTo(storeDefinition.getFiler());
+        } catch (IOException e) {
+            throw storeDefinition.newProcessingException(e);
+        }
     }
 
     private static FieldSpec buildField() {

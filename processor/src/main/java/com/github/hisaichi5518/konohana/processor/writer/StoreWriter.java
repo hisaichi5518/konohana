72079e5de88703ea524c283c2da39644abd11f29
hisaichi5518 <hisaichi5518@gmail.com>
2017-09-20 15:49:20 +0900

    Add StoreWriter class

---
+++ processor/src/main/java/com/github/hisaichi5518/konohana/processor/writer/StoreWriter.java
@@ -0,0 +1,9 @@
+package com.github.hisaichi5518.konohana.processor.writer;
+
+import com.github.hisaichi5518.konohana.processor.definition.StoreDefinition;
+
+public class StoreWriter {
+
+    public static void write(StoreDefinition storeDefinition) {
+    }
+}

b2320cbd9e43a3548d9708261ac1a7cfa2a6d699
hisaichi5518 <hisaichi5518@gmail.com>
2017-09-20 18:10:31 +0900

    Add JavaTypes class

---
+++ processor/src/main/java/com/github/hisaichi5518/konohana/processor/types/JavaTypes.java
@@ -0,0 +1,9 @@
+package com.github.hisaichi5518.konohana.processor.types;
+
+import com.squareup.javapoet.ClassName;
+
+public class JavaTypes {
+    public static final ClassName String = ClassName.get(String.class);
+
+    public static final ClassName Exception = ClassName.get(Exception.class);
+}

c99be1cdb8994e59c6a474356c9621b45b084d98
hisaichi5518 <hisaichi5518@gmail.com>
2017-09-20 17:23:31 +0900

    Add Key annotation

---
+++ annotation/src/main/java/com/github/hisaichi5518/konohana/annotation/Key.java
@@ -0,0 +1,15 @@
+package com.github.hisaichi5518.konohana.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+public @interface Key {
+    String name() default "";
+
+    String defaultValue() default "";
+
+    Class<?> prefsAdapter() default UseBuildInPrefsAdapter.class;
+
+    class UseBuildInPrefsAdapter {}
+}

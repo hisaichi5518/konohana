e14b2078e4c17d28dfc60db01cef5523ac6af334
hisaichi5518 <hisaichi5518@gmail.com>
2017-09-20 16:31:51 +0900

    Use new ArrayList<>()

--- processor/src/main/java/com/github/hisaichi5518/konohana/processor/builder/StoreMethods.java
+++ processor/src/main/java/com/github/hisaichi5518/konohana/processor/builder/StoreMethods.java
@@ -3,14 +3,14 @@ package com.github.hisaichi5518.konohana.processor.builder;
 import com.github.hisaichi5518.konohana.processor.definition.StoreDefinition;
 import com.squareup.javapoet.MethodSpec;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.lang.model.element.Modifier;
 
 public class StoreMethods {
     public static List<MethodSpec> build(StoreDefinition storeDefinition) {
-        List<MethodSpec> specs = Collections.emptyList();
+        List<MethodSpec> specs = new ArrayList<>();
 
         specs.add(MethodSpec.methodBuilder("removeAll")
                 .addModifiers(Modifier.PUBLIC)

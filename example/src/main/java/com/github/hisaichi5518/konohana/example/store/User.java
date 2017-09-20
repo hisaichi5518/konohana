33f705605957db66d384f36ff1d29954021cd497
hisaichi5518 <hisaichi5518@gmail.com>
2017-09-20 17:24:14 +0900

    Use @Key at example

--- example/src/main/java/com/github/hisaichi5518/konohana/example/store/User.java
+++ example/src/main/java/com/github/hisaichi5518/konohana/example/store/User.java
@@ -1,7 +1,10 @@
 package com.github.hisaichi5518.konohana.example.store;
 
+import com.github.hisaichi5518.konohana.annotation.Key;
 import com.github.hisaichi5518.konohana.annotation.Store;
 
 @Store
 interface User {
+    @Key
+    String name = "default name";
 }

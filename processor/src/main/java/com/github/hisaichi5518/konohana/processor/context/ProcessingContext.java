cd214c6557d2cda18a74ef6e6ab461ba76c7d361
hisaichi5518 <hisaichi5518@gmail.com>
2017-09-20 15:36:37 +0900

    Add ProcessingContext class

---
+++ processor/src/main/java/com/github/hisaichi5518/konohana/processor/context/ProcessingContext.java
@@ -0,0 +1,26 @@
+package com.github.hisaichi5518.konohana.processor.context;
+
+import android.support.annotation.NonNull;
+
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.element.Element;
+import javax.tools.Diagnostic;
+
+public class ProcessingContext {
+    private final ProcessingEnvironment processingEnvironment;
+    private final RoundEnvironment roundEnvironment;
+
+    public ProcessingContext(@NonNull ProcessingEnvironment processingEnvironment, @NonNull RoundEnvironment roundEnvironment) {
+        this.processingEnvironment = processingEnvironment;
+        this.roundEnvironment = roundEnvironment;
+    }
+
+    public void note(@NonNull String message) {
+        processingEnvironment.getMessager().printMessage(Diagnostic.Kind.NOTE, message);
+    }
+
+    public void error(@NonNull String message, @NonNull Element element) {
+        processingEnvironment.getMessager().printMessage(Diagnostic.Kind.ERROR, message, element);
+    }
+}

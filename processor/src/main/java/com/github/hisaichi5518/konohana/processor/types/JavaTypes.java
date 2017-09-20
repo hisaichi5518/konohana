8c377a50541f0c7edf6c7a338dd8c4316969ae02
hisaichi5518 <hisaichi5518@gmail.com>
2017-09-20 21:21:04 +0900

    Add Set type to JavaTypes

--- processor/src/main/java/com/github/hisaichi5518/konohana/processor/types/JavaTypes.java
+++ processor/src/main/java/com/github/hisaichi5518/konohana/processor/types/JavaTypes.java
@@ -1,9 +1,18 @@
 package com.github.hisaichi5518.konohana.processor.types;
 
 import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.ParameterizedTypeName;
+
+import java.util.Set;
 
 public class JavaTypes {
     public static final ClassName String = ClassName.get(String.class);
 
     public static final ClassName Exception = ClassName.get(Exception.class);
+
+    public static final ClassName Set = ClassName.get(Set.class);
+
+    public static ParameterizedTypeName getSet(ClassName className) {
+        return ParameterizedTypeName.get(Set, className);
+    }
 }

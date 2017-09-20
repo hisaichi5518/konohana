7dd23888b2653313e70b91af4a9fee57e40d6ff9
hisaichi5518 <hisaichi5518@gmail.com>
2017-09-20 16:00:48 +0900

    Add Store fields builder

--- processor/src/main/java/com/github/hisaichi5518/konohana/processor/definition/StoreDefinition.java
+++ processor/src/main/java/com/github/hisaichi5518/konohana/processor/definition/StoreDefinition.java
@@ -2,16 +2,20 @@ package com.github.hisaichi5518.konohana.processor.definition;
 
 import com.github.hisaichi5518.konohana.annotation.Store;
 import com.github.hisaichi5518.konohana.processor.context.ProcessingContext;
+import com.squareup.javapoet.ClassName;
 
 import java.util.stream.Stream;
 
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
 
+
 public class StoreDefinition {
 
     private final ProcessingContext context;
     private final TypeElement element;
+    private final ClassName interfaceName;
+    private final ClassName storeClassName;
 
     public static Stream<StoreDefinition> createStream(ProcessingContext context) {
         return context.roundEnvironment.getElementsAnnotatedWith(Store.class)
@@ -23,6 +27,16 @@ public class StoreDefinition {
     private StoreDefinition(ProcessingContext context, TypeElement element) {
         this.context = context;
         this.element = element;
+
+        interfaceName = ClassName.get(element);
+        storeClassName = ClassName.get(interfaceName.packageName(), interfaceName.simpleName() + "_Store");
     }
 
+    public ClassName getStoreClassName() {
+        return storeClassName;
+    }
+
+    public ClassName getInterfaceName() {
+        return interfaceName;
+    }
 }
--- processor/src/main/java/com/github/hisaichi5518/konohana/processor/writer/StoreWriter.java
+++ processor/src/main/java/com/github/hisaichi5518/konohana/processor/writer/StoreWriter.java
@@ -1,9 +1,33 @@
 package com.github.hisaichi5518.konohana.processor.writer;
 
 import com.github.hisaichi5518.konohana.processor.definition.StoreDefinition;
+import com.github.hisaichi5518.konohana.processor.types.AndroidTypes;
+import com.squareup.javapoet.FieldSpec;
+import com.squareup.javapoet.TypeSpec;
+
+import java.util.Collections;
+import java.util.List;
+
+import javax.lang.model.element.Modifier;
 
 public class StoreWriter {
 
     public static void write(StoreDefinition storeDefinition) {
+
+        TypeSpec typeSpec = TypeSpec.classBuilder(storeDefinition.getStoreClassName())
+                .addSuperinterface(storeDefinition.getInterfaceName())
+                .addFields(buildFields(storeDefinition))
+                .build();
+    }
+
+    private static List<FieldSpec> buildFields(StoreDefinition storeDefinition) {
+        List<FieldSpec> specs = Collections.emptyList();
+
+        specs.add(FieldSpec.builder(AndroidTypes.SharedPreferences, "prefs")
+                .addModifiers(Modifier.PRIVATE, Modifier.FINAL)
+                .build());
+
+        return specs;
+
     }
 }

c7162b0ad0c11168e1071fa6ad68939002341cd9
hisaichi5518 <hisaichi5518@gmail.com>
2017-09-20 17:24:37 +0900

    Create getter methods

--- processor/src/main/java/com/github/hisaichi5518/konohana/processor/builder/StoreMethods.java
+++ processor/src/main/java/com/github/hisaichi5518/konohana/processor/builder/StoreMethods.java
@@ -1,6 +1,9 @@
 package com.github.hisaichi5518.konohana.processor.builder;
 
+import com.github.hisaichi5518.konohana.processor.definition.KeyDefinition;
 import com.github.hisaichi5518.konohana.processor.definition.StoreDefinition;
+import com.github.hisaichi5518.konohana.processor.types.AnnotationTypes;
+import com.github.hisaichi5518.konohana.processor.types.KonohanaTypes;
 import com.squareup.javapoet.MethodSpec;
 
 import java.util.ArrayList;
@@ -14,7 +17,21 @@ public class StoreMethods {
 
         specs.add(buildRemoveAllMethod());
 
+        storeDefinition.keyDefinitionStream().forEach(keyDefinition -> {
+            specs.add(buildGetterSpec(keyDefinition));
+        });
+
         return specs;
+    }
+
+    private static MethodSpec buildGetterSpec(KeyDefinition keyDefinition) {
+        return MethodSpec.methodBuilder(keyDefinition.getGetterName())
+                .returns(keyDefinition.getFieldTypeName())
+                .addModifiers(Modifier.PUBLIC)
+                .addAnnotation(AnnotationTypes.NonNull)
+                .addStatement("return $T.get(prefs, $S, $L)",
+                        /* TODO */ KonohanaTypes.StringPrefsAdapter, keyDefinition.getPrefsKeyName(), keyDefinition.getFieldName())
+                .build();
     }
 
     private static MethodSpec buildRemoveAllMethod() {

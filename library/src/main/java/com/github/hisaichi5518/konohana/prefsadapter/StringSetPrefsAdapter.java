3884da55223e8913183647daa1adb36c280f7b7d
hisaichi5518 <hisaichi5518@gmail.com>
2017-09-20 20:17:37 +0900

    Use @NonNull

--- library/src/main/java/com/github/hisaichi5518/konohana/prefsadapter/StringSetPrefsAdapter.java
+++ library/src/main/java/com/github/hisaichi5518/konohana/prefsadapter/StringSetPrefsAdapter.java
@@ -2,17 +2,15 @@ package com.github.hisaichi5518.konohana.prefsadapter;
 
 import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 
 import java.util.Set;
 
 public class StringSetPrefsAdapter {
-    public static Set<String> get(SharedPreferences prefs, @NonNull String key, @Nullable Set<String> defaultValue) {
+    public static Set<String> get(SharedPreferences prefs, @NonNull String key, @NonNull Set<String> defaultValue) {
         return prefs.getStringSet(key, defaultValue);
     }
 
-    public static void set(SharedPreferences prefs, @NonNull String key, @Nullable Set<String> value) {
+    public static void set(SharedPreferences prefs, @NonNull String key, @NonNull Set<String> value) {
         prefs.edit().putStringSet(key, value).apply();
     }
-
 }

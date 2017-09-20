11349fda854096dc74dd21d7c6597c96022b36c3
hisaichi5518 <hisaichi5518@gmail.com>
2017-09-20 20:05:46 +0900

    Delete useless variable

--- library/src/main/java/com/github/hisaichi5518/konohana/prefsadapter/StringPrefsAdapter.java
+++ library/src/main/java/com/github/hisaichi5518/konohana/prefsadapter/StringPrefsAdapter.java
@@ -6,8 +6,7 @@ import android.support.annotation.NonNull;
 public class StringPrefsAdapter {
     @NonNull
     public static String get(SharedPreferences prefs, @NonNull String key, @NonNull String defaultValue) {
-        String val = prefs.getString(key, defaultValue);
-        return val;
+        return prefs.getString(key, defaultValue);
     }
 
     public static void set(SharedPreferences prefs, @NonNull String key, @NonNull String value) {

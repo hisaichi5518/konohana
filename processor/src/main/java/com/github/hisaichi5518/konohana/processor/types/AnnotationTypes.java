29c213a481de2a4caead272e7ce76d87390bed1c
hisaichi5518 <hisaichi5518@gmail.com>
2017-09-20 16:01:15 +0900

    Add AnnotationTypes class

---
+++ processor/src/main/java/com/github/hisaichi5518/konohana/processor/types/AnnotationTypes.java
@@ -0,0 +1,7 @@
+package com.github.hisaichi5518.konohana.processor.types;
+
+import com.squareup.javapoet.ClassName;
+
+public class AnnotationTypes {
+    public static final ClassName NonNull = ClassName.get(android.support.annotation.NonNull.class);
+}

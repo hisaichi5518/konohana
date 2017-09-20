7fa6079711128795dd75607e191e418027060c11
hisaichi5518 <hisaichi5518@gmail.com>
2017-09-20 16:01:06 +0900

    Add AndroidTypes class

---
+++ processor/src/main/java/com/github/hisaichi5518/konohana/processor/types/AndroidTypes.java
@@ -0,0 +1,9 @@
+package com.github.hisaichi5518.konohana.processor.types;
+
+import com.squareup.javapoet.ClassName;
+
+public class AndroidTypes {
+    public static final ClassName SharedPreferences = ClassName.get("android.content", "SharedPreferences");
+
+    public static final ClassName Context = ClassName.get("android.content", "Context");
+}

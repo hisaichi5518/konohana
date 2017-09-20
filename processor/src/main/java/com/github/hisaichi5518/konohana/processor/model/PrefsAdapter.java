4dbda433c4ea60d957b8611fa1d6c293e1ef4c0d
hisaichi5518 <hisaichi5518@gmail.com>
2017-09-20 21:23:09 +0900

    Add PrefsAdapter class

---
+++ processor/src/main/java/com/github/hisaichi5518/konohana/processor/model/PrefsAdapter.java
@@ -0,0 +1,61 @@
+package com.github.hisaichi5518.konohana.processor.model;
+
+import android.support.annotation.Nullable;
+
+import com.github.hisaichi5518.konohana.processor.definition.KeyDefinition;
+import com.github.hisaichi5518.konohana.processor.types.JavaTypes;
+import com.github.hisaichi5518.konohana.processor.types.KonohanaTypes;
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.TypeName;
+
+import java.util.stream.Stream;
+
+public class PrefsAdapter {
+
+    private static final PrefsAdapter[] BUILD_IN = {
+            create(TypeName.INT, KonohanaTypes.IntegerPrefsAdapter),
+            create(TypeName.BOOLEAN, KonohanaTypes.BooleanPrefsAdapter),
+            create(TypeName.FLOAT, KonohanaTypes.FloatPrefsAdapter),
+            create(TypeName.LONG, KonohanaTypes.LongPrefsAdapter),
+
+            create(JavaTypes.String, KonohanaTypes.StringPrefsAdapter),
+            create(JavaTypes.getSet(JavaTypes.String), KonohanaTypes.StringSetPrefsAdapter)
+    };
+
+    private final TypeName target;
+    private final ClassName adapter;
+
+    private static PrefsAdapter create(TypeName target, ClassName adapter) {
+        return new PrefsAdapter(target, adapter);
+    }
+
+    @Nullable
+    public static TypeName find(KeyDefinition keyDefinition) {
+        TypeName customPrefsAdapterType = keyDefinition.getCustomPrefsAdapterType();
+        if (customPrefsAdapterType.equals(KonohanaTypes.UseBuildInPrefsAdapter)) {
+            // Use BuildIn Adapter
+            return Stream.of(BUILD_IN)
+                    .filter(prefsAdapter -> prefsAdapter.match(keyDefinition.getFieldTypeName()))
+                    .map(prefsAdapter -> prefsAdapter.adapter)
+                    .findFirst()
+                    .orElseGet(() -> {
+                        if (keyDefinition.isEnum()) {
+                            return KonohanaTypes.EnumPrefsAdapter;
+                        } else {
+                            return null;
+                        }
+                    });
+        } else {
+            return customPrefsAdapterType;
+        }
+    }
+
+    private boolean match(TypeName typeName) {
+        return target.equals(typeName);
+    }
+
+    private PrefsAdapter(TypeName target, ClassName adapter) {
+        this.target = target;
+        this.adapter = adapter;
+    }
+}
